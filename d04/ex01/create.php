21f1c81e975a39988518142632323942ca442ae3
e1jefe <dmitry.sheptun@gmail.com>
2018-04-10 14:43:06 +0300

    rush and 04

---
+++ d04/ex01/create.php
@@ -0,0 +1,26 @@
+<?php
+    if (!$_POST["login"] === "" || $_POST["passwd"] === "" || $_POST["submit"] !== "OK")
+    {
+        echo "ERROR\n";
+        return ;
+    }
+    if (!file_exists("../private"))
+        mkdir ("../private");
+    if (file_exists("../private/passwd"))
+    {
+        $arr = unserialize(file_get_contents("../private/passwd"));
+        foreach ($arr as $user)
+        {
+            if ($user["login"] === $_POST["login"])
+            {
+                echo "ERROR\n";
+                return ;
+            }
+        }
+    }
+    $tmp["login"] = $_POST["login"];
+    $tmp["passwd"] = hash('whirlpool', $_POST["passwd"]);
+    $arr[] = $tmp;
+    file_put_contents("../private/passwd", serialize($arr));
+    echo "OK\n";
+?>
---
+++ d04/ex02/modif.php
@@ -0,0 +1,24 @@
+<?php
+if ($_POST["login"] === "" || $_POST["oldpw"] === "" || $_POST["newpw"] === "" || $_POST["submit"] !== "OK")
+{
+    echo "ERROR\n";
+    return ;
+}
+$arr = unserialize(file_get_contents("../private/passwd"));
+$login = $_POST["login"];
+$oldpw = hash('whirlpool', $_POST["oldpw"]);
+$newpw = hash('whirlpool', $_POST["newpw"]);
+foreach ($arr as $user)
+{
+    if ($user["login"] === $login && $user["passwd"] === $oldpw && $oldpw !== $newpw)
+    {
+        $user["passwd"] = $newpw;
+        $arr[$user] = $user;
+        file_put_contents("../private/passwd", serialize($arr));
+        echo "OK\n";
+        return ;
+    }
+}
+echo "ERROR\n";
+return ;
+?>
